dddf4598e7ed7a9bfd246829f44df6521f536124
Jalluritharun <tharunjalluri@gmail.com>
2026-02-27 16:38:02 +0530

    Added UC2 - Print a Hardcoded Palindrome Result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,43 @@
-
 public class PalindromeCheckerApp {
 
     /**
      * Application entry point.
-     * This is the first method executed by the JVM
-     * when the program starts.
      *
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
 
-        // Display Welcome Message
+        // ===== UC1: Welcome Message =====
         System.out.println("Welcome to the Palindrome Checker Management System");
-
-        // Display Version Information
         System.out.println("Version:");
-        System.out.println("1.0");
-
-        // Display System Initialization Message
+        System.out.println("2.0");
         System.out.println("System initialized successfully.");
+        System.out.println("--------------------------------------");
+
+        // ===== UC2: Hardcoded Palindrome Check =====
+
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("Application execution completed.");
     }
 }
